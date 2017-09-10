7f7e967d3c5bd3f6427bea162302eb5cd8f93129
Leo Nistor <leo.nistor@gmail.com>
2017-09-10 15:24:12 +0300

    code

--- flickr.js
+++ flickr.js
@@ -9,11 +9,40 @@ require([
   'ramda',
   'jquery'
 ], function (_, $) {
-  var trace = _.curry(function (tag, x) {
+  // Utils
+  var Impure = {
+    getJSON: _.curry(function (callback, url) {
+      $.getJSON(url, callback)
+    }),
+
+    setHtml: _.curry(function (sel, html) {
+      $(sel).html(html)
+    })
+  }
+
+  var trace = _.curry(function (tag, x) { // eslint-disable-line no-unused-vars
     console.log(tag, x)
     return x
   })
 
-  // app
-  console.log('go planet!')
+  var img = function (url) {
+    return $('<img />', {
+      src: url
+    })
+  }
+
+  // App
+  var url = function (term) {
+    return 'https://api.flickr.com/services/feeds/photos_public.gne?tags=' +
+      term + '&format=json&jsoncallback=?'
+  }
+
+  var mediaUrl = _.compose(_.prop('m'), _.prop('media'))
+  var mediaToImg = _.compose(img, mediaUrl)
+  var images = _.compose(_.map(mediaToImg), _.prop('items'))
+  var renderImages = _.compose(Impure.setHtml('body'), images)
+  // var app = _.compose(Impure.getJSON(trace('response')), url)
+  var app = _.compose(Impure.getJSON(renderImages), url)
+
+  app('cats')
 })
