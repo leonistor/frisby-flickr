c676ad0ac8bc9c0db0769d9b89ed2815e3e0ea25
Leo Nistor <leo.nistor@gmail.com>
2017-09-10 13:56:47 +0300

    prepros, libs

---
+++ flickr.js
@@ -0,0 +1,19 @@
+requirejs.config({
+  paths: { // fara .js la sfarsit!!
+    'ramda': '/node_modules/ramda/dist/ramda.min',
+    'jquery': '/node_modules/jquery/dist/jquery.min'
+  }
+})
+
+require([
+  'ramda',
+  'jquery'
+], function (_, $) {
+  var trace = _.curry(function (tag, x) {
+    console.log(tag, x)
+    return x
+  })
+
+  // app
+  console.log('go planet!')
+})
